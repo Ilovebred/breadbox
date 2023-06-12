7e79bf8b23396bedda5ce68c19c738324d289886
Ilovebred <113945168+Ilovebred@users.noreply.github.com>
2023-06-12 14:38:08 -0700

    P5 KD
    HEE HEE HEE HAWW. added some new Hud elements, and a proper game over screen.

--- Prototype5KD5/Assets/Scripts/Target.cs
+++ Prototype5KD5/Assets/Scripts/Target.cs
@@ -12,7 +12,7 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
 
-public ParticleSystem explosionParticle;
+    public ParticleSystem explosionParticle;
     public int PointValue;
 
     // Start is called before the first frame update
@@ -35,14 +35,24 @@ public ParticleSystem explosionParticle;
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
-        gameManager.UpdateScore(PointValue);
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(PointValue);
+        }
+
+        
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+          gameManager.GameOver();
+        }
     }
 
 
